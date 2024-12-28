776e812f9fa213ba005f5bb5cadb82d8967d4bac
Daniel Lee <danielsoft2024@gmail.com>
2024-12-28 07:48:21 -0800

    portfolio

---
+++ src/pages/skills.js
@@ -0,0 +1,10 @@
+import CardBox from "../components/Box/CardBox";
+export default function Skills(){
+    return(
+        <>
+            <span className="text-2xl md:text-5xl font-black tracking-widest text-blue-900">SKILLS</span>
+            <hr className="border-blue-900 m-3 lg:m-5"></hr>
+            <CardBox />
+        </>
+    )
+}
